697cdbaf8b1583fc010f1b37fd424df7917cedec
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-16 23:17:26 +0530

    Array

---
+++ Array/kth_Root.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class Kth_Root {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n = 150;
+        int k = 2;
+        System.out.println(KthRoot(n, k));
+
+    }
+
+    public static int KthRoot(int n, int k) {
+        int lo = 1;
+        int hi = n;
+        int ans = 0;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (Math.pow(mid, k) <= n) {
+                // we are updating ans here and modifying lo because if we find this if check
+                // true but we need the highest one who can satisfy this if check and always
+                // thing this will not go in an infinte loop because somewhere lo will be higher
+                // than hi and loop will exit
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+}
