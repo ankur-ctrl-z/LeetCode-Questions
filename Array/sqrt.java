8287035bc6605fc0c23ea619d949fe4e82ccbee0
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-16 18:15:21 +0530

    Array

---
+++ Array/sqrt.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class sqrt {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(mySqrt(n));
+    }
+
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
