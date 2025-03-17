0a3db2c83d0859fcd9e2fd6fa7caa7543ebd0032
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-17 19:16:41 +0530

    Array

---
+++ String/Valid_Palindrome.java
@@ -0,0 +1,21 @@
+public
+package String;
+
+class Valid_Palindrome {
+    public boolean isPalindrome(String s) {
+        String str = "";
+        str = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
