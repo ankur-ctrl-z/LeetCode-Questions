a84875ea3a0861b64b16782de4ee2826cb027c9d
ANKUR SHARMA <ankur143513@gmail.com>
2025-03-22 23:11:17 +0530

    Array

---
+++ Array/Maximum_Subarray.java
@@ -0,0 +1,18 @@
+public class Maximum_Subarray {
+    class Solution {
+        public int maxSubArray(int[] arr) {
+            int ans = Integer.MIN_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                int sum = 0;
+                for (int j = i; j < arr.length; j++) {
+                    sum += arr[j];
+                    ans = Math.max(ans, sum);
+                }
+                if (sum < 0) {
+                    sum = 0;
+                }
+            }
+            return ans;
+        }
+    }
+}
