94a5cf463f15761f8f8e3cb181486d4aa0a7ea5d
ANKUR SHARMA <ankur143513@gmail.com>
2025-04-29 17:08:41 +0530

    Update First_Bad_Version.java

--- Array/First_Bad_Version.java
+++ Array/First_Bad_Version.java
@@ -1,7 +1,7 @@
 package Array; 
  
 public class First_Bad_Version { 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         // TODO Auto-generated method stub
         System.out.println(firstBadVersion(9));
     }
