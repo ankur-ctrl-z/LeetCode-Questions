6bb2f88f6a46c829adbdf226943ea8c77881ebfc
ANKUR SHARMA <ankur143513@gmail.com>
2025-07-12 11:53:39 +0530

    0045-jump-game-ii

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int maxReach = 0;
+        int steps = 0;
+        int lastJumpIndex = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (i == lastJumpIndex) {
+                steps++;
+                lastJumpIndex = maxReach;
+            }
+        }
+
+        return steps;
+    }
+}
