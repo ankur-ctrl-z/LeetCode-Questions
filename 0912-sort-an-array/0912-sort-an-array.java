9229e47f69a87f80430eda1c16017ff240f37d85
ANKUR SHARMA <ankur143513@gmail.com>
2025-07-13 19:56:52 +0530

    Medium 28 ms (50.06%) 56 MB (61.33%)

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,40 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
+
+    private void mergeSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            merge(nums, low, mid, high);
+        }
+    }
+
+    private void merge(int[] nums, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) left[i] = nums[low + i];
+        for (int j = 0; j < n2; j++) right[j] = nums[mid + 1 + j];
+
+        int i = 0, j = 0, k = low;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                nums[k++] = left[i++];
+            } else {
+                nums[k++] = right[j++];
+            }
+        }
+
+        while (i < n1) nums[k++] = left[i++];
+        while (j < n2) nums[k++] = right[j++];
+    }
 }
+
+    
