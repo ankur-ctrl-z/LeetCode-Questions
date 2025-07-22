afffc2f7f8a9a375239d2cc9ed4f0480f0d3cce0
ANKUR SHARMA <ankur143513@gmail.com>
2025-07-22 19:12:47 +0530

    Medium 0 ms (100%) 43.5 MB (26.2%)

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,31 +1,35 @@
-class Solution { 
-    public boolean search(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length-1;
+class Solution {
+   public boolean search(int[] arr, int target) {
+    int low = 0, high = arr.length - 1;
 
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] == target){
-                return true;
-            }
-            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target) return true;
+
+        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
             low++;
             high--;
         }
-            if(arr[low] <= arr[mid]){
-                if(target >= arr[low] && target < arr[mid]) {
-                    high = mid - 1;
-                }else {
-                    low = mid + 1;
-                }
+
+        else if (arr[low] <= arr[mid]) {
+            if (target >= arr[low] && target < arr[mid]) {
+                high = mid - 1;
             } else {
-                if(target > mid && target <= arr[high]){
-                    low = mid + 1;
-                }else {
-                    high = mid - 1;
-                }
+                low = mid + 1;
             }
         }
-        return false;
+
+        else {
+            if (target > arr[mid] && target <= arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
     }
+
+    return false;
+}
+
 }
