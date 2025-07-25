b2fe4cb1ea33b242119336e0ac8a82cbd5b64695
ANKUR SHARMA <ankur143513@gmail.com>
2025-07-25 11:29:18 +0530

    Medium 181 ms (60.4%) 93 MB (67.52%)

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,54 @@
+class WordDictionary {
+
+    private class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new TrieNode();
+            }
+            node = node.children[idx];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int i, TrieNode node) {
+        if (node == null) return false;
+        if (i == word.length()) return node.isEnd;
+
+        char ch = word.charAt(i);
+        if (ch == '.') {
+            for (TrieNode child : node.children) {
+                if (child != null && dfs(word, i + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return dfs(word, i + 1, node.children[ch - 'a']);
+        }
+    }
+}
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
