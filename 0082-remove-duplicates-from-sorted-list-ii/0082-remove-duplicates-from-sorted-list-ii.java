1f28cb52196b0db0b13c581db563611c116a96db
ANKUR SHARMA <ankur143513@gmail.com>
2025-08-02 01:15:04 +0530

    Medium 0 ms (100%) 43.5 MB (25.75%)

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        ListNode dummy = new ListNode(0, head); 
+        ListNode slow = dummy;
+        ListNode fast = head;
+
+        while (fast != null) {
+            while (fast.next != null && fast.val == fast.next.val) {
+                fast = fast.next;
+            }
+
+            if (slow.next != fast) {
+                slow.next = fast.next;
+            } else {
+                slow = slow.next;
+            }
+
+            fast = fast.next;
+        }
+
+        return dummy.next;
+    }
+}
