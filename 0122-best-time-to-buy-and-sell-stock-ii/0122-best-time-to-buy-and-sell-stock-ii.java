839063e3c4b2f4d995a0ead492829d0a630c93a5
ANKUR SHARMA <ankur143513@gmail.com>
2025-08-09 23:58:34 +0530

    Medium 1 ms (77.36%) 45.7 MB (72.36%)

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,31 +1,42 @@
+// class Solution {
+//     public int maxProfit(int[] arr) {
+//         int pick = arr[0];   
+//         int profit = 0;
+//         int j = 1;           
+
+//         while (j < arr.length) {
+//             while (j < arr.length && arr[j] <= pick) {
+//                 pick = arr[j];
+//                 j++;
+//             }
+
+//             int sell = pick;
+//             while (j < arr.length && arr[j] >= sell) {
+//                 sell = arr[j];
+//                 j++;
+//             }
+
+//             profit += sell - pick;
+
+//             if (j < arr.length) {
+//                 pick = arr[j];
+//             }
+//         }
+//         return profit;
+//     }
+// }
+
+
+
 class Solution {
-    public int maxProfit(int[] arr) {
-        int pick = arr[0];   // Current buying price
+    public int maxProfit(int[] prices) {
         int profit = 0;
-        int j = 1;           // Start from second day
-
-        while (j < arr.length) {
-            // Find the next buying point (valley)
-            while (j < arr.length && arr[j] <= pick) {
-                pick = arr[j];
-                j++;
-            }
-
-            // Find the selling point (peak)
-            int sell = pick;
-            while (j < arr.length && arr[j] >= sell) {
-                sell = arr[j];
-                j++;
-            }
-
-            // Add profit from this transaction
-            profit += sell - pick;
-
-            // Set pick for the next transaction if not at the end
-            if (j < arr.length) {
-                pick = arr[j];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
             }
         }
         return profit;
     }
 }
+
