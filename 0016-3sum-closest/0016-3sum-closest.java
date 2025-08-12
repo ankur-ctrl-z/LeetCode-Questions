d0d80a6efbf317f74fc5f150a9db220f6e93ff2e
ANKUR SHARMA <ankur143513@gmail.com>
2025-08-12 23:09:43 +0530

    Medium 14 ms (71.65%) 43.5 MB (23.66%)

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int threeSumClosest(int[] arr, int target) {
+        Arrays.sort(arr); // sort first
+        int result = arr[0] + arr[1] + arr[2]; 
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (Math.abs(target - sum) < Math.abs(target - result)) {
+                    result = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum; 
+                }
+            }
+        }
+
+        return result;
+    }
+}
