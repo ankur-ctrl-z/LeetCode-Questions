22816f072fbd7e205b20a56265d49f60f6b221a2
ANKUR SHARMA <ankur143513@gmail.com>
2025-08-20 22:13:39 +0530

    Time: 1 ms (100%), Space: 42.1 MB (83.99%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+        boolean[] inStack = new boolean[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        StringBuilder stack = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']--;
+
+            if (inStack[c - 'a']) continue;
+
+            while (stack.length() > 0 &&
+                   stack.charAt(stack.length() - 1) > c &&
+                   freq[stack.charAt(stack.length() - 1) - 'a'] > 0) {
+                inStack[stack.charAt(stack.length() - 1) - 'a'] = false;
+                stack.deleteCharAt(stack.length() - 1);
+            }
+
+            stack.append(c);
+            inStack[c - 'a'] = true;
+        }
+
+        return stack.toString();
+    }
+}
