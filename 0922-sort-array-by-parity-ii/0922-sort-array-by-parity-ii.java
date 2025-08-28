5a5ddf629d40e43c322635dd738b830ab9af544a
ANKUR SHARMA <ankur143513@gmail.com>
2025-08-29 00:05:14 +0530

    Time: 4 ms (15.96%), Space: 47.5 MB (36.39%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -2,18 +2,23 @@ class Solution {
     public int[] sortArrayByParityII(int[] arr) {
        int i =0;
        int j = 1;
-       while(j < arr.length -1; && i < arr.length){
+       while(j < arr.length && i < arr.length){
           if(isEven(arr[i])){
-            arr[i] = arr[i];
             i = i+2;
-          }else{
-            arr[j] = arr[i];
-            j = j+2;
+          } else if(!isEven(arr[j])){
+            j = j + 2;
+          }
+          else{
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i +=2;
+            j+= 2;
           }
        }
        return arr;
+    }   
        public static boolean isEven(int num){
         return (num%2 == 0) ? true : false;
        }
-    }
 }
