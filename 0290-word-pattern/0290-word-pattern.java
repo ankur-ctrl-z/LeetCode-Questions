d90008485846b8c96e6e37db21f9d8ab143bbb6d
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-02 15:31:33 +0530

    Time: 1 ms (79.45%), Space: 41.8 MB (18.27%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+        if (pattern.length() != arr.length) return false;
+
+        HashMap<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            char ch = pattern.charAt(i);
+
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(arr[i])) {
+                    return false; 
+                }
+            } else {
+                if (map.containsValue(arr[i])) {
+                    return false;
+                }
+                map.put(ch, arr[i]);
+            }
+        }
+        return true; 
+    }
+}
