928cccee7dff18d7cd8ad7b8bc2fdf40d02d3c26
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-03 14:41:24 +0530

    Time: 24 ms (64.48%), Space: 45.2 MB (85.21%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,17 +1,42 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         StringBuilder sb = new StringBuilder(s);
+//         for(int i =0; i < sb.length()-1; i++){
+//             if (sb.charAt(i+1) != sb.charAt(i)) {
+//                 continue;
+//             }else {
+//                 sb.deleteCharAt(i);
+//                 sb.deleteCharAt(i);
+
+//                 i = i-2;
+//                 if (i < -1) i = -1;
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
+
+
+
+
+
 class Solution {
     public String removeDuplicates(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        for(int i =0; i < sb.length()-1; i++){
-            if (sb.charAt(i+1) != sb.charAt(i)) {
-                continue;
-            }else {
-                sb.deleteCharAt(i);
-                sb.deleteCharAt(i);
+        Stack<Character> st = new Stack<>();
 
-                i = i-2;
-                if (i < -1) i = -1;
+        for (char ch : s.toCharArray()) {
+            if (!st.isEmpty() && st.peek() == ch) {
+                st.pop(); 
+            } else {
+                st.push(ch); 
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : st) {
+            sb.append(c);
+        }
+
         return sb.toString();
     }
 }
