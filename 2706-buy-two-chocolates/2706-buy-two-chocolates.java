a5d528085850d549751fa5d04390d4c1da4322cd
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-06 22:43:30 +0530

    Time: 1 ms (100%), Space: 44.3 MB (70.44%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,36 @@
+// class Solution {
+//     public int buyChoco(int[] arr, int money) {
+//         Arrays.sort(arr);
+//         int left = money;
+//         for(int i = 0; i <= 1; i++){
+//             left = left-arr[i]; 
+//         }
+//         if(left >= 0){
+//             return left;
+//         }
+//         return money;
+//     }
+// }
+
+
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;  
+        int min2 = Integer.MAX_VALUE; 
+
+        for (int price : prices) {
+            if (price < min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+
+        int total = min1 + min2;
+        if (total <= money) {
+            return money - total;
+        }
+        return money;
+    }
+}
