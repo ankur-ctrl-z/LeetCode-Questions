1e3b468d3cf2e08ffc64f3c61e445238e52b3ac3
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-10 17:04:16 +0530

    Time: 56 ms (55.17%), Space: 59.6 MB (50%) - LeetHub

---
+++ 1733-minimum-number-of-people-to-teach/1733-minimum-number-of-people-to-teach.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        Set<Integer>[] userLangs = new Set[languages.length + 1];
+        for (int i = 0; i < languages.length; i++) {
+            userLangs[i + 1] = new HashSet<>();
+            for (int lang : languages[i]) {
+                userLangs[i + 1].add(lang);
+            }
+        }
+        
+        Set<Integer> problemUsers = new HashSet<>();
+        for (int[] friendship : friendships) {
+            int u = friendship[0], v = friendship[1];
+            boolean canCommunicate = false;
+            for (int lang : userLangs[u]) {
+                if (userLangs[v].contains(lang)) {
+                    canCommunicate = true;
+                    break;
+                }
+            }
+            if (!canCommunicate) {
+                problemUsers.add(u);
+                problemUsers.add(v);
+            }
+        }
+        
+        if (problemUsers.isEmpty()) return 0;
+        
+        int minTeachings = problemUsers.size();
+        for (int lang = 1; lang <= n; lang++) {
+            int teachings = 0;
+            for (int user : problemUsers) {
+                if (!userLangs[user].contains(lang)) {
+                    teachings++;
+                }
+            }
+            minTeachings = Math.min(minTeachings, teachings);
+        }
+        
+        return minTeachings;
+    }
+}
