97e3f492c3273c76a3c3f06534ed295649794b97
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-11 17:50:32 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.59%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -1,22 +1,36 @@
-class Solution {
-    public String largestGoodInteger(String s) {
-        ArrayList<Character> ll = new ArrayList<>();
-        int i = 0;
-        int j = 1;
-        int k = 2;
+// class Solution {
+//     public String largestGoodInteger(String s) {
+//         ArrayList<Character> ll = new ArrayList<>();
+//         int i = 0;
+//         int j = 1;
+//         int k = 2;
 
-        while(k < s.length()){
-            if(s.charAt(i) == s.charAt(j) && s.charAt(i) == s.charAt(k)){
-               ll.add(s.charAt(i));
+//         while(k < s.length()){
+//             if(s.charAt(i) == s.charAt(j) && s.charAt(i) == s.charAt(k)){
+//                ll.add(s.charAt(i));
+//             }
+//             i++;
+//             j++;
+//             k++;
+//         }
+//         Collections.sort(ll);
+//         if(ll.isEmpty()) return "";
+//         char num = ll.get(ll.size()-1);
+//         String number = num+""+num+""+num;
+//         return number;
+//     }
+// }
+
+
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        String[] str={"999","888","777","666","555","444","333","222","111","000"};
+        for(String s:str){
+            if(num.contains(s)){
+                return s;
             }
-            i++;
-            j++;
-            k++;
         }
-        Collections.sort(ll);
-        if(ll.isEmpty()) return "";
-        char num = ll.get(ll.size()-1);
-        String number = num+""+num+""+num;
-        return number;
+        return "";
     }
 }
