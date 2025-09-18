14083f3d997b5856a3039aaf1a610f8a10a32206
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-18 16:27:56 +0530

    Time: 87 ms (12.88%), Space: 52.2 MB (29.39%) - LeetHub

---
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int equalPairs(int[][] arr) {
+        int count = 0;
+        for(int i =0; i < arr.length; i++){
+            int[] row = arr[i];
+            for(int j =  0; j < arr[0].length; j++){
+                int[] col = new int[arr.length];
+               for(int k = 0; k < arr.length; k++){
+                 col[k] = arr[k][j];
+               } 
+               if(isSame(row,col)) count++;
+            }
+        }
+        return count;
+    }
+    public static boolean isSame(int[] a, int[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
