cd33eb7fd3ce89a533b1e7c329d64e83c2e6fd21
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-19 13:31:31 +0530

    Time: 9 ms (14.17%), Space: 44.5 MB (46.31%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,26 +1,8 @@
-// class Solution {
-//     public String removeOccurrences(String s, String part) {
-//         while(s.contains(part)){
-//            s = s.replaceFirst(part, "");
-//         }
-//         return s;
-//     }
-// }
-
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder sb = new StringBuilder(s);
-        int len = part.length();
-
-        while(true)
-        {
-            int ind = sb.indexOf(part);
-            if(ind==-1)
-            {
-                break;
-            }
-            sb.delete(ind,ind+len);
+        while(s.contains(part)){
+           s = s.replaceFirst(part, "");
         }
-        return sb.toString();
+        return s;
     }
 }
