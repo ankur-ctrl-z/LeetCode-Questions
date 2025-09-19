b6e6f7656dd0b9f723a540b1306a3e7c6e20d669
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-19 17:34:08 +0530

    Time: 0 ms (100%), Space: 41.3 MB (36.15%) - LeetHub

---
+++ 1026-string-without-aaa-or-bbb/1026-string-without-aaa-or-bbb.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        StringBuilder st = new StringBuilder();
+        while(a > 0 || b > 0){
+            if(a > b){
+                if(a >= 2){
+                    st.append("aa");
+                    a-=2;
+                } else {
+                    st.append('a');
+                    a--;
+                } if(b > 0){
+                    st.append('b');
+                    b--;
+                }
+            } else if(b > a){
+                if(b >= 2){
+                    st.append("bb");
+                    b -= 2;
+                } else{
+                    st.append('b');
+                    b--;
+                }
+                if(a > 0){
+                    st.append('a');
+                    a--;
+                }
+            } else {
+                st.append("ab");
+                a--;
+                b--;
+            }
+        }
+        return st.toString();
+    }
+}
