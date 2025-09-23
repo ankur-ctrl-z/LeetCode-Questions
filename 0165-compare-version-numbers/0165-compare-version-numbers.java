f7798f1c2d179c41823e69ea343daa779867daa6
ANKUR SHARMA <ankur143513@gmail.com>
2025-09-23 18:46:24 +0530

    Time: 1 ms (76.03%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,56 +1,55 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        ArrayList<Integer> parts1 = new ArrayList<>();
-        ArrayList<Integer> parts2 = new ArrayList<>();
+        ArrayList<Integer> ll1 = new ArrayList<>();
+        ArrayList<Integer> ll2 = new ArrayList<>();
 
-        StringBuilder temp = new StringBuilder();
-
-        for (int i = 0; i < version1.length(); i++) {
+        StringBuilder st = new StringBuilder();
+        for(int i = 0; i < version1.length(); i++){
             char ch = version1.charAt(i);
-            if (ch != '.') {
-                temp.append(ch);
-            } else {
-                parts1.add(Integer.parseInt(temp.toString()));
-                temp.setLength(0);
+            if(ch != '.'){
+                st.append(ch);
+            }else {
+                ll1.add(Integer.parseInt(st.toString()));
+                st.setLength(0);
             }
         }
-        if (temp.length() > 0) parts1.add(Integer.parseInt(temp.toString()));
+        if(st.length() > 0) {
+            ll1.add(Integer.parseInt(st.toString()));
+        }
 
-        temp.setLength(0);
+        st.setLength(0);
 
-        for (int i = 0; i < version2.length(); i++) {
+        for(int i = 0; i < version2.length(); i++){
             char ch = version2.charAt(i);
-            if (ch != '.') {
-                temp.append(ch);
-            } else {
-                parts2.add(Integer.parseInt(temp.toString()));
-                temp.setLength(0);
+            if(ch != '.'){
+                st.append(ch);
+            }else {
+                ll2.add(Integer.parseInt(st.toString()));
+                st.setLength(0);
             }
         }
-        if (temp.length() > 0) parts2.add(Integer.parseInt(temp.toString()));
+        if(st.length() > 0) {
+            ll2.add(Integer.parseInt(st.toString()));
+        }
 
-        int n = Math.max(parts1.size(), parts2.size());
-
-        for (int i = 0; i < n; i++) {
+        int n = Math.max(ll1.size(), ll2.size());
+        for(int i = 0; i < n; i++){
             int l1 = 0;
             int l2 = 0;
-            if(i < parts1.size()){
-                l1 = parts1.get(i);
-            } else {
+            if(i < ll1.size()){
+                l1 = ll1.get(i);
+            }else {
                 l1 = 0;
             }
-
-            if(i < parts2.size()){
-                l2 = parts2.get(i);
-            } else {
+            if(i < ll2.size()){
+                l2 = ll2.get(i);
+            }else {
                 l2 = 0;
             }
-            
-            if(l1 == l2) continue;
-            if (l1 < l2) return -1;
-            if (l1 > l2) return 1;
-        }
 
+            if(l1 < l2) return -1;
+            if(l1 > l2) return 1;
+        }
         return 0;
     }
 }
