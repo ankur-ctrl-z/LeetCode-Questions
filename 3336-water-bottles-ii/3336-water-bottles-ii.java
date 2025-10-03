3fe904076d7cc276023d4a03ae3ee2d207b846a4
ANKUR SHARMA <ankur143513@gmail.com>
2025-10-03 21:30:19 +0530

    Time: 0 ms (100%), Space: 40.7 MB (84.46%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int total = numBottles;   
+        int exchange = numBottles; 
+
+        while (exchange >= numExchange) {
+            exchange -= numExchange;  
+            numExchange++;           
+            total++;                
+            exchange++;               
+        }
+
+        return total;
+    }
+}
