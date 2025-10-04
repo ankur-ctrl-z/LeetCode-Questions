3951925fd616700981bc7abf73587ecafc3d34c4
ANKUR SHARMA <ankur143513@gmail.com>
2025-10-04 20:03:17 +0530

    Medium 0 ms (100%) 41.6 MB (8.69%)

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,19 +2,21 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return robber(nums, 0, dp);
+        Arrays.fill(dp,-1);
+        return solve(nums,0,dp);
+    
     }
-
-    public static int robber(int arr[], int i, int dp[]) {
-        if (i >= arr.length) {
+    public static int solve(int arr[], int i,int dp[]){
+        if(i >= arr.length){
             return 0;
         }
-        if (dp[i] != -1) {
+        if(dp[i] != -1){
             return dp[i];
         }
-        int rob = arr[i] + robber(arr, i + 2, dp);
-        int notrob = robber(arr, i + 1, dp);
-        return dp[i] = Math.max(rob, notrob);
+        int a = arr[i] + solve(arr,i+2,dp);
+        int b = solve(arr,i+1,dp);
+
+        dp[i] = Math.max(a,b);
+        return dp[i];
     }
 }
