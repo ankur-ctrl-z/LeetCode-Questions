d98f41252355fb25ccac32fb6ef0d7fe446f1bc4
ANKUR SHARMA <ankur143513@gmail.com>
2025-10-12 22:57:54 +0530

    Time: 39 ms (79.52%), Space: 60.5 MB (77.77%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length == 1 || nums.length == 0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int diff = 0;
+        int a = 0;
+        int b = 1;
+        while(b < nums.length){
+            if(nums[b] - nums[a] > diff){
+                diff = nums[b] - nums[a];
+                a++;
+                b++;
+            } else {
+                a++;
+                b++;
+            }
+        }
+        return diff;
+    }
+}
