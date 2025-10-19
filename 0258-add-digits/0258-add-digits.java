42ea74eb88dd1d9dad1dfb58d4c905e8fba3d1b5
ANKUR SHARMA <ankur143513@gmail.com>
2025-10-20 00:02:55 +0530

    Time: 1 ms (44.12%), Space: 41.1 MB (33.34%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -12,18 +12,3 @@ class Solution {
         return num;
     }
 }
-// class Solution {
-//     public int addDigits(int num) {
-//         int sum=0;
-//         while(num>=10){
-//             sum=0;
-//             while(num>0){
-//                 int temp=num%10;
-//                 sum+=temp;
-//                 num/=10;
-//             }
-//             num=sum;
-//         }
-//         return num;
-//     }
-// }
