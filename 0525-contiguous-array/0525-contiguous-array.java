1d3fcbff74233193979324d0838f4bf6ef4852e2
ANKUR SHARMA <ankur143513@gmail.com>
2025-10-27 23:42:54 +0530

    Time: 18 ms (97.35%), Space: 55.1 MB (76.14%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -16,21 +16,25 @@ class Solution {
             arr[i] = 1;
         }
       }
+
+
    for (int i = 0; i<arr.length; i++){
        sum+=arr[i];
        if (sum==0){
            maxi= i+1;
+
        }
        else {
            if (map.containsKey(sum)){
                maxi= Math.max(maxi, i- map.get(sum));
-          }
+
+           }
            else {
                map.put(sum,i);
            }
        }
 
-    }
+   }
       return maxi;
     }
 }
