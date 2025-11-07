1341a1cc3fda6b311b02315e8a85d8a43d2e6521
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-07 23:26:25 +0530

    Update 0016-3sum-closest.java

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int threeSumClosest(int[] arr, int target) {
+    public int threeSumClosest(int[] arr, int target) { 
         Arrays.sort(arr); // sort first
         int result = arr[0] + arr[1] + arr[2]; 
 
