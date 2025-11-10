aac2b670db118bbba937da23a4d4cae88b833347
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-10 20:55:58 +0530

    Medium 38 ms (21.22%) 48.5 MB (5.55%)

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> {
+                int diffA = Math.abs(a - x);
+                int diffB = Math.abs(b - x);
+                if (diffA == diffB) return a - b; 
+                return diffA - diffB; 
+            }
+        );
+
+        for (int num : arr) pq.offer(num);
+
+        List<Integer> result = new ArrayList<>();
+        while (k > 0 && !pq.isEmpty()) {
+            result.add(pq.poll());
+            k--;
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+}
+
+
