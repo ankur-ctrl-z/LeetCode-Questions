b552169b57d65d7f503fa456c73a48607ae12d66
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-12 19:57:24 +0530

    Medium 1 ms (100%) 43.9 MB (8.22%)

--- 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.java
+++ 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.java
@@ -1,23 +1,32 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int count = 0;
-        boolean odd = false;
-        int isone = 0;
-        boolean one = false;
-        for(int num : nums){
-            if(num % 2 != 0){
-                odd = true;
-            } else if(num == 1){
-                one = true;
-                isone++;
+        int n = nums.length;
+
+        int countOfOnes = 0;
+        for (int num : nums) {
+            if (num == 1) countOfOnes++;
+        }
+        if (countOfOnes > 0) return n - countOfOnes;
+
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int currentGcd = nums[i];
+            for (int j = i; j < n; j++) {
+                currentGcd = gcd(currentGcd, nums[j]);
+                if (currentGcd == 1) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
             }
         }
-        if(odd){
-            return nums.length;
-        } else if(one) {
-            return nums.length - isone;
-        } else {
-            return -1;
-        }
+
+        if (minLen == Integer.MAX_VALUE) return -1;
+
+        return (minLen - 1) + (n - 1);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
