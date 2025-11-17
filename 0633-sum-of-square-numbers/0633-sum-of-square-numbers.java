0bc29f4bce86f816e200bd1ec3797bd5e9d3f3db
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-17 22:58:22 +0530

    Medium 4 ms (52.19%) 42.2 MB (7.78%)

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
         long low = 0;
-        long high = (long) Math.sqrt(c);
+        long high = (int) Math.sqrt(c);
 
         while (low <= high) {
             long sum = low * low + high * high;
