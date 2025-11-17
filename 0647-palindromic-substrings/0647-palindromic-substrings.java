32c1355f99f6a97161b86996cfc75f7513e2f1e5
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-17 23:37:15 +0530

    Medium 6 ms (66.83%) 41.5 MB (79.99%)

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int countSubstrings(String str) {
+    public int countSubstrings(String s) {
+        int n = s.length();
         int count = 0;
-    for (int i = 0; i < str.length(); i++) {
-        for (int j = i; j < str.length(); j++) {
-            String sub = str.substring(i, j + 1);
-            if(ispalindrome(sub)) count++;
+
+        // Each character can be a center of palindrome (odd and even length)
+        for (int center = 0; center < 2 * n - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            // Expand while it's palindrome
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                count++;
+                left--;
+                right++;
             }
         }
+
         return count;
-    }
-    public static boolean ispalindrome(String s){
-        if(s.length() == 1) return true;
-        int i = 0;
-        int j = s.length()-1;
-        while(i <= j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            } 
-            i++;
-            j--;
-        }
-        return true;
     }
 }
