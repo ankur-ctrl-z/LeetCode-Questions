679dabd43a5e7a94d2aaf2c68ce5b0de9a1fd282
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-02 20:12:43 +0530

    Easy 8 ms (42.25%) 40.2 MB (95.36%)

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,8 @@
 class Solution {
     public int fib(int n) {
-        int[] memo = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            memo[i] = -1;
+        if(n == 0 || n ==1){
+            return n;
         }
-        return fibo(n,memo);
+        return fib(n-1) + fib(n-2);
     }
-    public static int fibo(int n, int memo[]){
-        if(n <= 1) return n;
-
-        if(memo[n] != -1){
-            return memo[n];
-        }
-        memo[n] = fibo(n-1,memo) + fibo(n-2,memo);
-        return memo[n];
-    }   
 }
