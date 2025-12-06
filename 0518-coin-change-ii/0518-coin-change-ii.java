98950d6f5c98ac6ffdcae6682e2b31372342b8d5
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-06 23:38:08 +0530

    Medium 37 ms (6.92%) 54.8 MB (38.22%)

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,14 +1,25 @@
-public class Solution {
+class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-        
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] += dp[j - coin];
-            }
+        int n = coins.length;
+        Integer[][] dp = new Integer[n][amount + 1];
+        return solve(0, amount, coins, dp);
+    }
+
+    private int solve(int idx, int amount, int[] coins, Integer[][] dp) {
+        if (amount == 0) return 1;
+        if (idx == coins.length) return 0;
+
+        if (dp[idx][amount] != null) return dp[idx][amount];
+
+        int ways = 0;
+
+        if (amount >= coins[idx]) {
+            ways += solve(idx, amount - coins[idx], coins, dp);
         }
-        
-        return dp[amount];
+
+        ways += solve(idx + 1, amount, coins, dp);
+
+        return dp[idx][amount] = ways;
     }
 }
+
