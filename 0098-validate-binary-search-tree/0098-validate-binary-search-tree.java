ec985de82cf15b37266326f3723b5b1513b4b813
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-13 21:23:14 +0530

    Medium 2 ms (12.3%) 45.7 MB (6.25%)

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.ArrayList;
+
+class Solution {
+    private ArrayList<Integer> ll = new ArrayList<>();
+    
+    public boolean isValidBST(TreeNode root) {
+        ll.clear(); 
+        inorder(root);
+
+        for (int i = 0; i < ll.size() - 1; i++) {
+            if (ll.get(i) >= ll.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        ll.add(node.val);
+        inorder(node.right);
+    }
+}
