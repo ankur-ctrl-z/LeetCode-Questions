bcf7bfbfee7bc3628ac5ad8cbcc2aacd7ed905df
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-15 19:08:33 +0530

    Easy 1 ms (38%) 45 MB (12.54%)

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,24 +2,14 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int dp[] = new int[cost.length];
         Arrays.fill(dp,-1);
-        return Math.min(
-            solve(cost, 0,dp),
-            solve(cost, 1,dp)
-        );
+       return Math.min(minimum(cost,0,dp),minimum(cost,1,dp)); 
     }
-
-    private int solve(int[] cost, int idx, int dp[]) {
-        if (idx >= cost.length) {
-            return 0;
-        }
+    public static int minimum(int arr[], int idx,int dp[]){
+        if(idx >= arr.length) return 0 ;
         if(dp[idx] != -1) return dp[idx];
-        int one = solve(cost, idx + 1, dp);
-        int two = solve(cost, idx + 2, dp);
+        int a = arr[idx] + minimum(arr,idx+2,dp);
+        int b = arr[idx] + minimum(arr,idx+1,dp);
 
-        dp[idx] =  cost[idx] + Math.min(one, two);
-        return dp[idx];
+        return dp[idx] = Math.min(a,b);
     }
 }
-
-
-
