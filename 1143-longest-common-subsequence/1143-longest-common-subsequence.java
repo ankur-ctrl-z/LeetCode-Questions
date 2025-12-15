97c3f21f22f91a9000e7472902bded0d76c0c7ed
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-15 19:41:54 +0530

    Medium 29 ms (17.39%) 54.1 MB (21.17%)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,8 +1,8 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int dp[][] = new int[text1.length()+1][text2.length()+1];
-        for (int i = 0; i <= text1.length(); i++) {
-         for(int j = 0; j <= text2.length(); j++){
+        for (int i = 1; i <= text1.length(); i++) {
+         for(int j = 1; j <= text2.length(); j++){
             dp[i][j] = -1;
          }
         }
