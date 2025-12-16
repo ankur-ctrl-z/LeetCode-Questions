d01afd2b1f4a31c2b7084732a314e1a351390239
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-16 19:16:25 +0530

    Medium 3 ms (96.06%) 47.2 MB (33.51%)

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,23 +1,35 @@
 class Solution {
-    public int minDistance(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        int dp[][] = new int [n+1][m+1];
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-            for(int j = 0; j <= m ; j++){
-                dp[0][j] = j;
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // initialize with -1
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = -1;
             }
         }
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                } else {
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]);
-                }
-            }
+
+        return solve(word1, word2, n, m, dp);
+    }
+
+    private int solve(String s1, String s2, int i, int j, int[][] dp) {
+        if (i == 0) return j;
+        if (j == 0) return i;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            return dp[i][j] = solve(s1, s2, i - 1, j - 1, dp);
         }
-        return dp[n][m];
+
+        int insert  = solve(s1, s2, i, j - 1, dp);
+        int delete  = solve(s1, s2, i - 1, j, dp);
+        int replace = solve(s1, s2, i - 1, j - 1, dp);
+
+        return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
     }
 }
