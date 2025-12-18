c969b03d8e16ffc28bf86017dccf085d63b85911
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-18 19:20:21 +0530

    Time: 1 ms (72.91%), Space: 43.8 MB (5.66%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,17 +13,6 @@
  *     }
  * }
  */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -34,20 +23,18 @@ class Solution {
 
         while (!q.isEmpty()) {
             int size = q.size();
-            TreeNode rightMost = null;
 
-            // process all nodes of current level
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                rightMost = node; // last node in this level
+                
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
 
                 if (node.left != null) q.add(node.left);
                 if (node.right != null) q.add(node.right);
             }
-
-            result.add(rightMost.val);
         }
-
         return result;
     }
 }
