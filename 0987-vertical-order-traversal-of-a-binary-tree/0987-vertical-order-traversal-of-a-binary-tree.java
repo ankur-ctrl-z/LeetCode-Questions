796412cfcadaa80878847919f603f313a8d2978e
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-01 22:41:16 +0530

    Time: 5 ms (16.19%), Space: 44.6 MB (8.32%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    static class Pair {
+        TreeNode node;
+        int row;
+        int col;
+
+        Pair(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 0, 0));
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            TreeNode node = p.node;
+            int row = p.row;
+            int col = p.col;
+
+            if (node == null) continue;
+
+            map
+                .computeIfAbsent(col, k -> new TreeMap<>())
+                .computeIfAbsent(row, k -> new PriorityQueue<>())
+                .add(node.val);
+
+            q.add(new Pair(node.left, row + 1, col - 1));
+            q.add(new Pair(node.right, row + 1, col + 1));
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
+            List<Integer> vertical = new ArrayList<>();
+            for (PriorityQueue<Integer> pq : rows.values()) {
+                while (!pq.isEmpty()) {
+                    vertical.add(pq.poll());
+                }
+            }
+            result.add(vertical);
+        }
+
+        return result;
+    }
+}
