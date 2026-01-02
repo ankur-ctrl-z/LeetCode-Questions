85194f47466129bf304f63076b29844bd9b196be
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-02 19:10:25 +0530

    Time: 37 ms (28.22%), Space: 59.2 MB (45.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,7 +6,6 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < N - 2; i++) {
-            if (nums[i] > 0) break;
             
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
