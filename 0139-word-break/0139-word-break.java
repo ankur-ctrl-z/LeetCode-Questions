72a4004417c0f31f590b7cba351aa50dd486edc2
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-05 18:42:25 +0530

    Time: 9 ms (24.23%), Space: 46.4 MB (19.44%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,24 +1,16 @@
 class Solution {
-    public boolean wordBreak(String target, List<String> arr) {
-        Boolean[] dp = new Boolean[target.length()];
-        return dfs(arr, target, 0, dp);
-    }
-
-    private boolean dfs(List<String> arr, String target, int idx, Boolean[] dp) {
-        if (idx == target.length()) return true;
-        if (dp[idx] != null) return dp[idx];
-
-        for (String word : arr) {
-            int len = word.length();
-            if (idx + len <= target.length() &&
-                target.startsWith(word, idx)) {
-
-                if (dfs(arr, target, idx + len, dp)) {
-                    return dp[idx] = true;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        return dp[idx] = false;
+        return dp[s.length()];
     }
 }
-
