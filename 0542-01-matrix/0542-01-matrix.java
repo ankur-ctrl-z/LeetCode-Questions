b97e6f60ba24ae85c6d3cf14b4c441fc1a8b29f7
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-10 20:42:00 +0530

    Time: 14 ms (77.6%), Space: 48.7 MB (84.55%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,37 +1,35 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
+        if (mat == null || mat.length == 0 || mat[0].length == 0)
+            return new int[0][0];
 
-        // Step 1: Add all 0s to queue
+        int m = mat.length, n = mat[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int MAX_VALUE = m * n;
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
-                    q.add(new int[]{i, j});
-                    visited[i][j] = true;
+                    queue.offer(new int[]{i, j});
+                } else {
+                    mat[i][j] = MAX_VALUE;
                 }
             }
         }
-
-        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}}; // 4 directions
-
-        // Step 2: BFS
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            for (int[] dir : dirs) {
-                int newRow = cell[0] + dir[0];
-                int newCol = cell[1] + dir[1];
-
-                if (newRow >=0 && newRow < m && newCol >=0 && newCol < n && !visited[newRow][newCol]) {
-                    mat[newRow][newCol] = mat[cell[0]][cell[1]] + 1;
-                    visited[newRow][newCol] = true;
-                    q.add(new int[]{newRow, newCol});
+        
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] dir : directions) {
+                int r = cell[0] + dir[0], c = cell[1] + dir[1];
+                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[cell[0]][cell[1]] + 1) {
+                    queue.offer(new int[]{r, c});
+                    mat[r][c] = mat[cell[0]][cell[1]] + 1;
                 }
             }
         }
-
+        
         return mat;
     }
-}
 }
