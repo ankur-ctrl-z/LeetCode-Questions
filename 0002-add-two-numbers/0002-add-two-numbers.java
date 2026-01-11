b6d754530a2230a94295ddc85e0732f3b18edfce
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-11 21:51:36 +0530

    Update 0002-add-two-numbers.java

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -7,7 +7,7 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+ */ 
 /**
  * Definition for singly-linked list.
  * public class ListNode {
