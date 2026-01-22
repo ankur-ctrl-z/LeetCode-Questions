07abd7d6da8dd6cbecacfea3d165c333752f0648
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-23 00:10:57 +0530

    Time: 8 ms (15.32%), Space: 46.3 MB (15.27%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,21 +1,15 @@
-import java.util.Stack;
-
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<String> st = new Stack<>();
         
-        // Corrected loop to push all tokens onto the stack first
         for(int i = 0; i < tokens.length; i++){
            st.push(tokens[i]);
         }
 
-        // Using a temporary stack to process in the correct RPN order
         Stack<Integer> temp = new Stack<>();
 
-        // We iterate through the original stack exactly once
         for (String token : st) {
             if (token.length() == 1 && isOperators(token.charAt(0))) {
-                // When we hit an operator, we pop two from our number stack
                 int b = temp.pop(); 
                 int a = temp.pop();
                 char ch = token.charAt(0);
@@ -29,7 +23,6 @@ class Solution {
                 }
                 temp.push(result);
             } else {
-                // If it's a number, convert and store in the number stack
                 temp.push(Integer.parseInt(token));
             }
         }
