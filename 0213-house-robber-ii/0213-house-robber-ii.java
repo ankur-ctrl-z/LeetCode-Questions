6c8ee6a47e21a35ea34fbb4a1c2fc48accad31f9
ANKUR SHARMA <ankur143513@gmail.com>
2026-02-13 11:38:44 +0530

    Time: 0 ms (100%), Space: 43.1 MB (20.81%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return nums[0];
+        if(nums.length == 1) return nums[0];
+        int dp[] = new int[nums.length];
+        int dp2[] = new int[nums.length];
 
-        // Case 1: use houses 0 to n-2
-        int case1 = robLinear(nums, 0, n - 2);
+        Arrays.fill(dp,-1);
+        Arrays.fill(dp2,-1);
 
-        // Case 2: use houses 1 to n-1
-        int case2 = robLinear(nums, 1, n - 1);
-
-        return Math.max(case1, case2);
+        int result = Math.max(solvehouseRobber(nums,0,nums.length-1,dp), solvehouseRobber(nums,1,nums.length,dp2));
+        return result;
     }
-
-    private int robLinear(int[] arr, int start, int end) {
-        int prev = 0;  
-        int prev2 = 0; 
-
-        for (int i = start; i <= end; i++) {
-            int take = arr[i] + prev2;
-            int notTake = prev;
-            int curr = Math.max(take, notTake);
-
-            prev2 = prev;
-            prev = curr;
+    public static int solvehouseRobber(int arr[], int idx,int end, int dp[]) {
+        if(idx >= end){
+            return 0;
         }
-        return prev;
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        int a = arr[idx] + solvehouseRobber(arr,idx+2,end,dp);
+        int b = solvehouseRobber(arr,idx+1,end,dp);
+
+        return dp[idx] = Math.max(a,b);
     }
 }
