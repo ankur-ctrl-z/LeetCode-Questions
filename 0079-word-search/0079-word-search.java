d3a375593805ff9c55718664011b5bd4b9808f69
ANKUR SHARMA <ankur143513@gmail.com>
2026-02-13 12:19:28 +0530

    Time: 2716 ms (5%), Space: 47 MB (6.7%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,35 +1,38 @@
 class Solution {
+    static String ans = "";
+
     public boolean exist(char[][] arr, String word) {
         int m = arr.length, n = arr[0].length;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (isexist(i, j, 0, arr, word)) {
-                    return true;
-                }
+                if (isexist(i, j, arr, word)) return true;
             }
         }
         return false;
     }
 
-    public static boolean isexist(int i, int j, int idx, char[][] arr, String word) {
-        if (idx == word.length()) return true;
+    public static boolean isexist(int i, int j, char[][] arr, String word) {
+        if (ans.equals(word)) return true;
 
-        if (i < 0 || j < 0 || i >= arr.length || j >= arr[0].length ||
-            arr[i][j] != word.charAt(idx)) {
-            return false;
-        }
+        if (i < 0 || j < 0 || i >= arr.length || j >= arr[0].length) return false;
+        if (ans.length() >= word.length()) return false;
+        if (arr[i][j] != word.charAt(ans.length())) return false;
 
         char temp = arr[i][j];
+        ans += temp;
         arr[i][j] = '#';
 
         boolean found =
-                isexist(i + 1, j, idx + 1, arr, word) ||
-                isexist(i - 1, j, idx + 1, arr, word) ||
-                isexist(i, j + 1, idx + 1, arr, word) ||
-                isexist(i, j - 1, idx + 1, arr, word);
+                isexist(i + 1, j, arr, word) ||
+                isexist(i - 1, j, arr, word) ||
+                isexist(i, j + 1, arr, word) ||
+                isexist(i, j - 1, arr, word);
 
         arr[i][j] = temp;
+        ans = ans.substring(0, ans.length() - 1);
+
         return found;
     }
 }
+
