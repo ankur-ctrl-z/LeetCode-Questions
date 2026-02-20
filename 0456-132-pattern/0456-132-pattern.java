c24f29531a6bbb2a391ed897b570675a625d0ea1
ANKUR SHARMA <ankur143513@gmail.com>
2026-02-20 19:17:31 +0530

    Time: 40 ms (46.2%), Space: 99.2 MB (11.26%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,13 +1,14 @@
-class Solution {
+public class Solution {
     public boolean find132pattern(int[] nums) {
-        int num_i = nums[0];
-        for(int j = 1; j < nums.length-1; j++) {
-            num_i = Math.min(num_i, nums[j]);
-            for(int k = j + 1; k < nums.length; k++) {
-                if(num_i < nums[k] && nums[k] < nums[j]) {
-                    return true;
-                }
+        Stack<Integer> stack = new Stack<>();
+        int third = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < third) return true;
+            while (!stack.isEmpty() && stack.peek() < nums[i]) {
+                third = stack.pop();
             }
+            stack.push(nums[i]);
         }
         return false;
     }
