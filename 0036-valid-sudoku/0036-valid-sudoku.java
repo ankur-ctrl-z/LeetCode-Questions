6464d29934eeadb8bf271dbe58e2926205c3bbbc
ANKUR SHARMA <ankur143513@gmail.com>
2026-02-28 16:56:23 +0530

    Time: 3 ms (51.03%), Space: 46.5 MB (53.72%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,32 +3,31 @@ import java.util.*;
 class Solution {
     public boolean isValidSudoku(char[][] board) {
 
-        // Row check
         for (int i = 0; i < 9; i++) {
-            Set<Character> set = new HashSet<>();
+            Set<Character> rowSet = new HashSet<>();
+            Set<Character> colSet = new HashSet<>();
+
             for (int j = 0; j < 9; j++) {
-                char val = board[i][j];
-                if (val == '.') continue;
-                if (set.contains(val)) return false;
-                set.add(val);
+
+                char rowVal = board[i][j];
+                if (rowVal != '.') {
+                    if (rowSet.contains(rowVal)) return false;
+                    rowSet.add(rowVal);
+                }
+
+                char colVal = board[j][i];
+                if (colVal != '.') {
+                    if (colSet.contains(colVal)) return false;
+                    colSet.add(colVal);
+                }
             }
         }
 
-        // Column check
-        for (int j = 0; j < 9; j++) {
-            Set<Character> set = new HashSet<>();
-            for (int i = 0; i < 9; i++) {
-                char val = board[i][j];
-                if (val == '.') continue;
-                if (set.contains(val)) return false;
-                set.add(val);
-            }
-        }
-
-        // 3x3 grid check
         for (int r = 0; r < 9; r += 3) {
             for (int c = 0; c < 9; c += 3) {
+
                 Set<Character> set = new HashSet<>();
+
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
                         char val = board[r + i][c + j];
