5b1bc24f41e0f4336fec5ed76f3d060d3fc67dbb
ANKUR SHARMA <ankur143513@gmail.com>
2026-02-28 16:51:59 +0530

    Time: 3 ms (51.03%), Space: 46.5 MB (68.73%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,33 +1,40 @@
+import java.util.*;
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
 
+        // Row check
         for (int i = 0; i < 9; i++) {
             Set<Character> set = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if (c == '.') continue;
-                if (!set.add(c)) return false;
+                char val = board[i][j];
+                if (val == '.') continue;
+                if (set.contains(val)) return false;
+                set.add(val);
             }
         }
 
+        // Column check
         for (int j = 0; j < 9; j++) {
             Set<Character> set = new HashSet<>();
             for (int i = 0; i < 9; i++) {
-                char c = board[i][j];
-                if (c == '.') continue;
-                if (!set.add(c)) return false;
+                char val = board[i][j];
+                if (val == '.') continue;
+                if (set.contains(val)) return false;
+                set.add(val);
             }
         }
 
+        // 3x3 grid check
         for (int r = 0; r < 9; r += 3) {
             for (int c = 0; c < 9; c += 3) {
-
                 Set<Character> set = new HashSet<>();
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
                         char val = board[r + i][c + j];
                         if (val == '.') continue;
-                        if (!set.add(val)) return false;
+                        if (set.contains(val)) return false;
+                        set.add(val);
                     }
                 }
             }
