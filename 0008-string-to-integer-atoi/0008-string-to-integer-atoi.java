7835c3699a77648922852281fe0257416ca7b82d
ANKUR SHARMA <ankur143513@gmail.com>
2026-04-14 12:34:00 +0530

    Time: 7 ms (3.33%), Space: 45.8 MB (5.1%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+class Solution {
+    ArrayList<Character> ll = new ArrayList<>();
+
+    public Solution() {
+        for (int i = 0; i <= 9; i++) {
+            ll.add((char) (i + '0'));
+        }
+    }
+
+    public int myAtoi(String s) {
+        return checkTheString(s);
+    }
+
+    public int checkTheString(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        s = s.trim();
+        String result = "";
+
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+            result += s.charAt(0);
+        } else if (!ll.contains(s.charAt(0))) {
+            return 0;
+        }
+
+        for (int i = (s.charAt(0) == '-' || s.charAt(0) == '+') ? 1 : 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ll.contains(ch)) {
+                result += ch;
+            } else if (!result.equals("")) {
+                break;
+            } else {
+                return 0;
+            }
+        }
+
+        if (result.equals("") || result.equals("+") || result.equals("-")) {
+            return 0;
+        }
+
+        try {
+            return Integer.parseInt(result);
+        } catch (NumberFormatException e) {
+            return result.charAt(0) == '-' ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+    }
+}
