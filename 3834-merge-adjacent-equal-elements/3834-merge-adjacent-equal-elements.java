b28c29c592926df17ca1eabf5375f5079ad770b3
ANKUR SHARMA <ankur143513@gmail.com>
2026-05-06 23:38:42 +0530

    Time: 25 ms (76.1%), Space: 127.5 MB (73.68%) - LeetHub

---
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<Long> mergeAdjacent(int[] nums) {
+
+        List<Long> stack = new ArrayList<>();
+
+        for (int num : nums) {
+
+            stack.add((long) num);
+
+            while (stack.size() >= 2) {
+
+                int n = stack.size();
+
+                long last = stack.get(n - 1);
+                long secondLast = stack.get(n - 2);
+
+                if (last == secondLast) {
+
+                    stack.remove(n - 1);
+                    stack.remove(n - 2);
+
+                    stack.add(last + secondLast);
+
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return stack;
+    }
+}
