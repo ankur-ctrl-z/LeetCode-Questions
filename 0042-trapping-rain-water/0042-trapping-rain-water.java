de41cf7779831305084fe6af6ec9ef32a75222d4
ANKUR SHARMA <ankur143513@gmail.com>
2026-05-25 13:54:23 +0530

    Hard 1 ms (58.71%) 48.9 MB (8.98%)

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,34 @@
 class Solution {
-    public int trap(int[] height) {
-     return trapping(height); 
-    }
-    public static int trapping(int[] arr) {
-        int n = arr.length;
-        if(n==0) return 0;
-        int left[] = new int[n];
-        int right[] = new int[n];
+    public int trap(int[] arr) {
+        int left[] = new int[arr.length];
+        int right[] = new int[arr.length];
 
-        // finding left most maximum
+        int maxleft = arr[0];
+        int maxright = arr[arr.length-1];
         left[0] = arr[0];
-        for(int i =1; i < n; i++){
-            left[i] = Math.max(left[i-1],arr[i]);
-        }
-        right[n-1] = arr[n-1];
-        for(int j = n-2; j >= 0; j--){
-            right[j] = Math.max(arr[j],right[j+1]);
+
+        right[arr.length-1] = arr[arr.length-1];
+
+        for(int i = 1; i < arr.length; i++){
+            maxleft = Math.max(maxleft, arr[i]);
+            left[i] = maxleft;
         }
 
-        int sum = 0;
-        for(int k = 0; k < n; k++){
-            sum += Math.min(right[k],left[k])-arr[k];
+        for(int i = arr.length -2; i >= 0; i--){
+            maxright = Math.max(maxright,arr[i]);
+            right[i] = maxright;
         }
-        return sum;
+
+        for(int i = 0; i < left.length; i++){
+            int temp = arr[i];
+            arr[i] = Math.min(left[i],right[i]);
+            arr[i] = arr[i] - temp;
+        }
+
+        int result = 0;
+        for(int i = 0; i < arr.length; i++){
+            result += arr[i];
+        }
+        return result;
     }
-
 }
