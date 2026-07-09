f0553e221a4eb4da7dac0175fea3eae7eaea84ff
ANKUR SHARMA <ankur143513@gmail.com>
2026-07-09 23:47:52 +0530

    Medium 5 ms (8.94%) 81.5 MB (22.09%)

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -7,21 +7,16 @@ class Solution {
         while(j < nums.length){             
             boolean hasConflict = false;
             
-            // Check nums[j] against every element currently in your window
             for (int k = i; k < j; k++) {
                 if ((nums[k] & nums[j]) != 0) {
                     hasConflict = true;
-                    break; // Found a conflict!
+                    break; 
                 }
             }
             
             if (hasConflict) {                 
-                // If there's a conflict, shrink the window from the left by 1
                 i++; 
-                // DO NOT increment j here. Loop will run again and re-check 
-                // this same nums[j] against the newly shrunk window.
             } else {
-                // If it passes all elements, update max and move to the next number
                 max = Math.max(max, j - i + 1);             
                 j++;         
             }
