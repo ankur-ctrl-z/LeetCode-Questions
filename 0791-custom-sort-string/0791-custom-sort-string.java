9f24a0caff191f12cc3cb6467330367399a19f9c
ANKUR SHARMA <ankur143513@gmail.com>
2026-08-23 00:36:35 +0530

    Time: 4 ms (19.67%), Space: 43.6 MB (9.07%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public String customSortString(String order, String s) {
+
+        StringBuilder st2 = new StringBuilder(s);
+        String ans = "";
+
+        for (int i = 0; i < order.length(); i++) {
+
+            char ch = order.charAt(i);
+
+            int idx = st2.indexOf(String.valueOf(ch));
+
+            while (idx != -1) {
+                ans += ch;
+                st2.deleteCharAt(idx);
+
+                idx = st2.indexOf(String.valueOf(ch));
+            }
+        }
+
+        return ans + st2.toString();
+    }
+}
