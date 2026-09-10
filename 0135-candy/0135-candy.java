5894d182e47ac6672db8ed8534dc4a874b758902
ANKUR SHARMA <ankur143513@gmail.com>
2026-09-11 00:00:47 +0530

    Time: 3 ms (86.8%), Space: 52.6 MB (8.58%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,28 +1,42 @@
 class Solution {
     public int candy(int[] arr) {
         int n = arr.length;
-        if (n == 1) return 1;
+        if (n == 1)
+            return 1;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
 
-        int[] candy = new int[n];
-        Arrays.fill(candy, 1);
+        int i = 0;
+        int j = 1;
 
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > arr[i - 1]) {
-                candy[i] = candy[i - 1] + 1;
+        while (j < n) {
+            if (arr[i] < arr[j]) {
+                candies[j] = candies[i] + 1;
             }
+
+            i++;
+            j++;
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            if (arr[i] > arr[i + 1]) {
-                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+        i = n - 2;
+        j = n - 1;
+
+        while (i >= 0) {
+            if (arr[i] > arr[j]) {
+                candies[i] = Math.max(candies[i], candies[j] + 1);
             }
+
+            i--;
+            j--;
         }
 
         int ans = 0;
-        for (int x : candy) {
-            ans += x;
+
+        for (int candy : candies) {
+            ans += candy;
         }
 
         return ans;
     }
+
 }
